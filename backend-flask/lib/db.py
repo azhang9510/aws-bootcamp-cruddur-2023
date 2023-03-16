33ba005c2b5c3f54390b339f3bad57b725924bc0
azhang9510 <azhang9510@gmail.com>
2023-03-16 14:57:29 +0000

    db edits

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,7 +1,6 @@
 from psycopg_pool import ConnectionPool
 import os
-import re
-import sys
+
 
 class Db:
   def __init__(self):
@@ -10,31 +9,20 @@ class Db:
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
-  #when we want to commit data such as insert
-  #returning in all uppercase
-  def query_commit(self,sql, params):
-    print("SQL STATEMENT -[commit with returning ]---------")
-    print(sql + "\n")
-
-    pattern = r"\bRETURNING\b"
-    is_returning_id = re.search(pattern, sql)
-    
+  #when we want to commit data such as an insert
+  def query_commit(self):
     try:
       conn = self.pool.connection()
       cur = conn.cursor()
-      cur.execute(sql, params)
-      if is_returning_id:
-        returning_id = cur.fetchone()[0]
-      cunn.commit()
-      if is_returning_id:
-        return returning_id
-      return returning_id
+      cur.execute(sql)
+      conn.commit()
     except Exception as err:
-      self.print_sql_err(err)  
-  #when we want to return a json object
+      self.print_sql_err(err)
+      #conn.rollback()
+  #when we want to retunr JSON objecxt
   def query_array_json(self,sql):
-    print("SQL Statements--[Array]-----")
-    print(sql + "\n")
+    print("SQL----Statement--[array]---------")
+    print(sql +"\n")
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
@@ -43,14 +31,16 @@ class Db:
         return json[0]
   #when we want to return an array of json object
   def query_object_json(self,sql):
-    print("SQL Statements--[Object]-----")
+    print("SQL----Statement--[Object]---------")
+    print(sql +"\n")
     wrapped_sql = self.query_wrap_object(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(wrapped_sql)
         json = cur.fetchone()
+        return json[0]
 
-  def query_wrap_object(self,template):
+  def query_wrap_object(template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
     {template}
@@ -65,6 +55,7 @@ class Db:
     ) array_row);
     """
     return sql
+  # define a function that handles and parses psycopg2 exceptions
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
@@ -75,6 +66,9 @@ class Db:
     # print the connect() error
     print ("\npsycopg ERROR:", err, "on line number:", line_num)
     print ("psycopg traceback:", traceback, "-- type:", err_type)
+
+    # psycopg2 extensions.Diagnostics object attribute
+    print ("\nextensions.Diagnostics:", err.diag)
 
     # print the pgcode and pgerror exceptions
     print ("pgerror:", err.pgerror)
