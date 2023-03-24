c3d09a927fb6453dd10bde09d270c0639a1997e6
azhang9510 <azhang9510@gmail.com>
2023-03-24 18:52:04 +0000

    need to work on the scripts

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' ,'andrew@exampro','MOCK'),
-  ('Andrew Bayko', 'bayko' ,'bayko@bayko','MOCK');
+  ('Andrew Bayko', 'bayko' ,'bayko@bayko','MOCK')
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
