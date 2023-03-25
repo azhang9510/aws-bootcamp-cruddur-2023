4b66ad690b7db1ba1c92354f7e8fd274a3161361
azhang9510 <azhang9510@gmail.com>
2023-03-25 03:07:51 +0000

    still working of this. Trying to make it work at DynamoDB Stream video at 33.54

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro', 'andrewbrown' ,'MOCK'),
+  --('Andrew Brown','andrew@exampro', 'andrewbrown' ,'MOCK'),
   ('aubrey', 'azhang95100@gmail.com','azhang','MOCK'),
   ('Andrew Bayko', 'bayko@bayko','bayko' ,'MOCK'),
-  ('aubrey zhang',  'azhang9510@gmail.com','azhang9510', 'MOCK');
+  ('Londo Mollari','lmollario@centari.com','londo','MOCK'),;
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'azhang' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
